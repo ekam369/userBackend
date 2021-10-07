c90188ef027a42d08a63f25a7a65f795e22c0393
Ekamjit Singh <ekam.nugen@gmail.com>
2021-10-07 21:19:22 +0530

    backend request to get all users

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { saveUser, getUsers } = require('../controllers/user');
+const api = express.Router();
+
+api.route("/user")
+    .post(saveUser)
+    .get(getUsers);
+
+module.exports = api;
+
